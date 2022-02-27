68ac5faad8ba842a6d9b28da31fc69a600cc281a
morgaesis <91963632+morgaesis@users.noreply.github.com>
2022-02-27 18:52:16 +0000

    Update src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,5 @@
 int main() {
 	std::cout << "Hi there, dear world!" << std::endl;
 	std::cout << "And a good morning." << std::endl;
-	std::cout << "We should get implementing" << std::endl;
+	std::cout << "We should start implementing some features" << std::endl;
 }
