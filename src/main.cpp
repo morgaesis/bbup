123bbb95e0bba7cea293d65d95add6ddf12ac809
morgaesis <91963632+morgaesis@users.noreply.github.com>
2022-02-27 18:44:25 +0000

    Rearrange workflow files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main() {
-	std::cout << "Hi there, dear world :)" << std::endl;
+	std::cout << "Hi there, dear world!" << std::endl;
 	std::cout << "And a good morning" << std::endl;
 }
