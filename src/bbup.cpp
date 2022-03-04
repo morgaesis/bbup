333b68b036a6b69658391a3ea5a7bc19e5095aa4
Mörgæsis <91963632+morgaesis@users.noreply.github.com>
2022-03-04 15:11:51 +0000

    Add remote/local n-backup options

--- src/bbup.cpp
+++ src/bbup.cpp
@@ -6,10 +6,34 @@ namespace po = boost::program_options;
 int main(int argc, char* argv[]) {
 	po::options_description desc("Options");
 	std::cout << "Configuring options" << std::endl;
+
+	int local_n_quarter;
+	int local_n_hour;
+	int local_n_day;
+	int local_n_week;
+	int local_n_month;
+	int local_n_year;
+	int remote_n_quarter;
+	int remote_n_hour;
+	int remote_n_day;
+	int remote_n_week;
+	int remote_n_month;
+	int remote_n_year;
+
 	desc.add_options()
 		("help", "Show this help message")
-		("local-n-day", po::value<int>(), "How many versions of the last day to keep (locally)")
-		("remote-n-day", po::value<int>(), "How many versions of the last day to keep (remotely)")
+		("local-n-quarter", po::value<int>(&local_n_quarter)->default_value(4), "Number of quarterly backups to store (locally)")
+		("local-n-hour", po::value<int>(&local_n_hour)->default_value(24), "Number of hourly backups to store (locally)")
+		("local-n-day", po::value<int>(&local_n_day)->default_value(7), "Number of daily backups to store (locally)")
+		("local-n-week", po::value<int>(&local_n_week)->default_value(5), "Number of weekly backups to store (locally)")
+		("local-n-month", po::value<int>(&local_n_month)->default_value(12), "Number of monthly backups to store (locally)")
+		("local-n-year", po::value<int>(&local_n_year)->default_value(3), "Number of yearly backups to store (locally)")
+		("remote-n-quarter", po::value<int>(&remote_n_quarter)->default_value(4), "Number of quarterly backups to store (remotely)")
+		("remote-n-hour", po::value<int>(&remote_n_hour)->default_value(24), "Number of hourly backups to store (remotely)")
+		("remote-n-day", po::value<int>(&remote_n_day)->default_value(7), "Number of daily backups to store (remotely)")
+		("remote-n-week", po::value<int>(&remote_n_week)->default_value(5), "Number of weekly backups to store (remotely)")
+		("remote-n-month", po::value<int>(&remote_n_month)->default_value(12), "Number of monthly backups to store (remotely)")
+		("remote-n-year", po::value<int>(&remote_n_year)->default_value(3), "Number of yearly backups to store (remotely)")
 	;
 	po::variables_map options;
 	std::cout << "Parsing options" << std::endl;
@@ -23,8 +47,17 @@ int main(int argc, char* argv[]) {
 		return 1;
 	}
 
-	std::cout << "show local-n-day" << std::endl;
-	if (options.count("local-n-day")) {
-		std::cout << "local-n-day: " << options["local-n-day"].as<int>() << std::endl;
-	}
+	
+	std::cout << "local_n_quarter: " <<  local_n_quarter << std::endl;
+	std::cout << "local_n_hour: " <<  local_n_hour << std::endl;
+	std::cout << "local_n_day: " <<  local_n_day << std::endl;
+	std::cout << "local_n_week: " <<  local_n_week << std::endl;
+	std::cout << "local_n_month: " <<  local_n_month << std::endl;
+	std::cout << "local_n_year: " <<  local_n_year << std::endl;
+	std::cout << "remote_n_quarter: " <<  remote_n_quarter << std::endl;
+	std::cout << "remote_n_hour: " <<  remote_n_hour << std::endl;
+	std::cout << "remote_n_day: " <<  remote_n_day << std::endl;
+	std::cout << "remote_n_week: " <<  remote_n_week << std::endl;
+	std::cout << "remote_n_month: " <<  remote_n_month << std::endl;
+	std::cout << "remote_n_year: " <<  remote_n_year << std::endl;
 }
