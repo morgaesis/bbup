d28c15a1e90b753e576890ae1c266452099d9094
morgaesis <91963632+morgaesis@users.noreply.github.com>
2022-03-04 11:17:23 +0000

    Basic CLI

--- src/bbup.cpp
+++ src/bbup.cpp
@@ -3,6 +3,28 @@
 
 namespace po = boost::program_options;
 
-int main() {
-	system("echo \"I am $(whoami) in directory ${PWD}\"");
+int main(int argc, char* argv[]) {
+	po::options_description desc("Options");
+	std::cout << "Configuring options" << std::endl;
+	desc.add_options()
+		("help", "Show this help message")
+		("local-n-day", po::value<int>(), "How many versions of the last day to keep (locally)")
+		("remote-n-day", po::value<int>(), "How many versions of the last day to keep (remotely)")
+	;
+	po::variables_map options;
+	std::cout << "Parsing options" << std::endl;
+	po::store(po::parse_command_line(argc, argv, desc), options);
+	std::cout << "Notifying" << std::endl;
+	po::notify(options);
+
+	std::cout << "Print help?" << std::endl;
+	if (options.count("help")) {
+		std::cout << desc << std::endl;
+		return 1;
+	}
+
+	std::cout << "show local-n-day" << std::endl;
+	if (options.count("local-n-day")) {
+		std::cout << "local-n-day: " << options["local-n-day"].as<int>() << std::endl;
+	}
 }
